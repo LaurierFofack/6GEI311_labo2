efd7509bf0d2d852040914ab5b82848abc84c9df
Laurier <lfngoufack@etu.uqac.ca>
2024-10-02 00:02:17 -0400

    mise à jour adresse ip

--- labo2/app_laurier.py
+++ labo2/app_laurier.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/') 
 def hello_world() :
-    return '<h2>Hello, World! From laurier:  172.19.137.57 </h2>'
+    return '<h2>Hello, World! From laurier:  192.168.40.1 </h2>'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=3000, debug=True)
